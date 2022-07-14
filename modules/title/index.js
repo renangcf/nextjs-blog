b80321a92d379d359fe9d41cacff9c089c4b492a
maplepls <tiagococarv@gmail.com>
2022-07-14 11:25:26 -0300

    fix:adjusting title

--- modules/title/index.js
+++ modules/title/index.js
@@ -3,7 +3,7 @@ import styles from './styles.module.css';
 
 export default function Title() {
     return (
-        <h1 id={styles.title}>Wordle Clone 100% legit gamer!</h1>
+        <h1 id={styles.title}>Speedle!</h1>
     )
 }
 
